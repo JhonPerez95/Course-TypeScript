ae0cf67e3097b9a1472e98de609333c8d47b5d16
Jhon Perez <perezl.40nlsa.teleperformance.com>
2022-07-18 17:48:36 -0500

    iniciando con typescript

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+const a = 20;
+let b = 10;
+function sayHello(msg) {
+    console.log(msg);
+}
---
+++ app.ts
@@ -0,0 +1,7 @@
+const a: number = 20
+
+let b: number = 10
+
+function sayHello(msg:string) {
+  console.log(msg)
+}
