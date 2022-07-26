dcda3a4f8c26dd29513cc445adb4f70cc74b6493
Jhon Perez <perezl.40nlsa.teleperformance.com>
2022-07-26 09:08:19 -0500

    types tuplas and array

---
+++ tipos/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+;
+(() => {
+    const hero = ['Dr strange', 2, true];
+    hero[0] = 'iroman';
+    hero[1] = 50;
+    hero[2] = false;
+    console.log(hero);
+})();
---
+++ tipos/tuples.ts
@@ -0,0 +1,8 @@
+;(() => {
+  const hero: [string, number, boolean] = ['Dr strange', 2, true]
+
+  hero[0] = 'iroman'
+  hero[1] = 50
+  hero[2] = false
+  console.log(hero)
+})()
