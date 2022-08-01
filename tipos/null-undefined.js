087585e7852c940c12fb06539cf7c8259a73f7b0
jhonperez95 <jhonperez.j95@gmail.com>
2022-07-31 19:46:21 -0500

    terminando sesion de tipos

---
+++ tipos/null-undefined.js
@@ -0,0 +1,10 @@
+"use strict";
+;
+(() => {
+    let isActive = true;
+    let isOpen = undefined;
+    let isTrue = null;
+    console.log(isActive);
+    console.log(isOpen);
+    console.log(isTrue);
+})();
---
+++ tipos/null-undefined.ts
@@ -0,0 +1,10 @@
+;(() => {
+  let isActive: boolean = true
+
+  let isOpen: boolean | undefined = undefined
+  let isTrue: boolean | null = null
+
+  console.log(isActive)
+  console.log(isOpen)
+  console.log(isTrue)
+})()
